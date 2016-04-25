3e7e20f32095d70749e5142e674981abad99038d
Javier García Garrido <javiergarciagarrido@gmail.com>
2016-04-25 20:07:02 +0200

    read manuscript in challenge 2

--- challenge_2/solve.py
+++ challenge_2/solve.py
@@ -1,3 +1,8 @@
+def read_manuscript(name):
+    manuscript = open(name)
+    content = manuscript.readline()
+    return content.split()
 
 if __name__ == "__main__":
-    pass
+    manuscript_words = read_manuscript('corpussample.txt')
+    print manuscript_words[11-1:22].count("Blue")
